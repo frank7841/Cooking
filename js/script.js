d2cefff62d6b27a1c53f2e7d000d0f4d6945d879
Frank <franklinewax@gmail.com>
2020-05-07 11:49:24 +0300

    testing javascript connection

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function calc(grams,killoGrams){
+    killoGrams=parseInt(prompt("Input measurements in kg:"));
+    grams=killoGrams*1000;
+    alert(killoGrams +" kilos converted to grams is "+ grams +" grams");
+    }
+    calc();
+    
